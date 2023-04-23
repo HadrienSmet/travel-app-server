637e96a6cd4e1b28430fdd416d51c987528d4078
Hadrien <smethadrien.96@gmail.com>
2023-04-23 17:56:46 +0200

    Trying to use Google Cloud Storage to handle the files uploaded by the user because the files seams to disappear when they are stored on heroku

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const helmet = require('helmet');
-const path = require('path');
-const userRoutes = require('./routes/user');
+const express = require("express");
+const helmet = require("helmet");
+const path = require("path");
+const userRoutes = require("./routes/user");
 const postsRoutes = require("./routes/posts");
-require('./dbConfig');
+require("./dbConfig");
 
 const app = express();
 
@@ -11,19 +11,25 @@ app.use(helmet());
 
 //Sets the proper headers to avoid errors from CORS policy
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
     res.setHeader("Cross-Origin-Resource-Policy", "*");
     next();
-  });
+});
 
 app.use(express.json());
 
-app.use('/api', postsRoutes);
-app.use('/api/auth', userRoutes);
+app.use("/api", postsRoutes);
+app.use("/api/auth", userRoutes);
 
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 module.exports = app;
--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,4 +1,29 @@
+// const multer = require("multer");
+
+// const MIME_TYPES = {
+//     "image/jpg": "jpg",
+//     "image/jpeg": "jpg",
+//     "image/png": "png",
+// };
+
+// const storage = multer.diskStorage({
+//     destination: (req, file, callback) => {
+//         callback(null, "images");
+//     },
+//     filename: (req, file, callback) => {
+//         const name = file.originalname.split(" ").join("_");
+//         const extension = MIME_TYPES[file.mimetype];
+//         const nameWithoutExtension = name.split(".")[0];
+//         callback(null, nameWithoutExtension + Date.now() + "." + extension);
+//     },
+// });
+// console.log("multer: " + storage);
+
+// module.exports = multer({ storage }).any("file");
+
 const multer = require("multer");
+const { Storage } = require("@google-cloud/storage");
+const path = require("path");
 
 const MIME_TYPES = {
     "image/jpg": "jpg",
@@ -6,17 +31,63 @@ const MIME_TYPES = {
     "image/png": "png",
 };
 
-const storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, "images");
-    },
-    filename: (req, file, callback) => {
-        const name = file.originalname.split(" ").join("_");
-        const extension = MIME_TYPES[file.mimetype];
-        const nameWithoutExtension = name.split(".")[0];
-        callback(null, nameWithoutExtension + Date.now() + "." + extension);
-    },
+const googleCloud = new Storage({
+    keyFilename: path.join(
+        __dirname,
+        "./positive-nuance-384615-ccbb6d20f605.json"
+    ),
+    projectId: "positive-nuance-384615",
 });
-console.log("multer: " + storage);
 
-module.exports = multer({ storage }).any("file");
+const gcFiles = googleCloud.bucket("travel-app-bucket");
+
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, cb) => {
+    if (MIME_TYPES[file.mimetype]) {
+        cb(null, true);
+    } else {
+        cb(new Error("Invalid file type."), false);
+    }
+};
+
+const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+});
+
+const uploadMiddleware = upload.any("file");
+
+const uploadToGCS = (file) => {
+    const blob = gcFiles.file(file.originalname);
+    const stream = blob.createWriteStream({
+        metadata: {
+            contentType: file.mimetype,
+        },
+    });
+    stream.on("error", (err) => {
+        console.error(`Error uploading file ${file.originalname}:`, err);
+    });
+    stream.on("finish", () => {
+        console.log(`File ${file.originalname} uploaded.`);
+        // Supprimez le fichier une fois qu'il a été téléchargé
+        fs.unlink(file.path, (err) => {
+            if (err) {
+                console.error(err);
+            }
+        });
+    });
+    stream.end(file.buffer);
+};
+
+module.exports = (req, res, next) => {
+    uploadMiddleware(req, res, (err) => {
+        if (err) {
+            return res.status(400).json({ error: err.message });
+        }
+        req.files.forEach((file) => {
+            uploadToGCS(file);
+        });
+        next();
+    });
+};
